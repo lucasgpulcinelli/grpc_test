5561ab5e2c0037908027cdc5c9c0d739f9789dd9
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2022-04-19 14:47:36 -0300

    Bruteforce way for docker to run

--- functions/functions_grpc.pb.go
+++ functions/functions_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v3.19.4
 // source: functions.proto
 
-package grpc_test
+package functions
 
 import (
 	context "context"
---
+++ server/build.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/lucasgpulcinelli/grpc_test
+cd grpc_test/server
+go build .
+mv server /server
